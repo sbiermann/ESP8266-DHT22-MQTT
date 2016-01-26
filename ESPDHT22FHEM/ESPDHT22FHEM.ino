51d5ec5f0eed15f275e42242caad59e890cdbdf9
sbn <sb@ems-solutions.com>
2016-01-26 22:02:05 +0100

    -removing not needed parts -moving code to start, no need for loop method -tuning to run as fast as possible to save energy

--- ESPDHT22FHEM/ESPDHT22FHEM.ino
+++ ESPDHT22FHEM/ESPDHT22FHEM.ino
@@ -20,15 +20,7 @@ extern "C" {
 #include "mem.h"
 #include "user_interface.h"
 #include "cont.h"
-uint16 readvdd33(void);
 }
-
-/*************************************************/
-/* Deep Sleep                                    */
-/*************************************************/
-// Please see also: http://forum.fhem.de/index.php/topic,35572.msg279312.html#msg279312
-// 
-const bool DeepSleep = true;  // set to true to activate deep sleep
 
 /*************************************************/
 /* Debugging                                     */
@@ -44,7 +36,7 @@ const bool debugOutput = true;  // set to true for serial OUTPUT
 
 #define SERIAL_SPEED 115200
 
-#define DHT22_PIN 2       // Pin 2 as 1-wire bus for DHT22
+#define DHT22_PIN 12       // Pin 12 as 1-wire bus for DHT22
 
 /*************************************************/
 /* Settings for WLAN                             */
@@ -55,7 +47,6 @@ const char* password = "supergeheimespasswort";
 /*************************************************/
 /* Static IP                                     */
 /*************************************************/
-const bool dhcp = false;         // set to true for DHCP
 IPAddress ip(192,168,1,210);
 IPAddress gateway(192,168,1,254);
 IPAddress subnet(255,255,255,0);
@@ -73,68 +64,42 @@ int  mqttPort = 1883;
 
 #define TOPIC_ClientID "ESP8266/" MQTTDeviceID
 #define TOPIC_LastWill  TOPIC_ClientID "/connected"
-#define TOPIC_CounterState    TOPIC_ClientID "/counter/value"
 #define TOPIC_HumidityState  TOPIC_ClientID "/humidity/value"
 #define TOPIC_TemperatureState  TOPIC_ClientID "/temperature/value"
-
-
-// stati der mqtt statemachine
-#define ST_INIT 0
-#define ST_CONNECT 2
-#define ST_CONNECTED 3
-#define ST_DISTRIBUTED 4
-
-bool firstInit = true;
 
 // Create an WiFiClient object, here called "ethClient":
 WiFiClient ethClient;
 
-//mqtt client verbindung definieren, samt callback
-PubSubClient client(ethClient, mqttBroker, mqttPort); // instanziierung ueber DNS Namen
+//Create MQTT client object
+PubSubClient client(ethClient, mqttBroker, mqttPort);
 
 // Create an DHT object, here called "dht":
 DHT dht;
-unsigned long dhtWaiter;
-bool tempReq=false;
-bool humReq=false;
-float lastTemp;
-float lastHum;
-
-int MqState = 0;
-unsigned long MqWaiter; 
-boolean callbackEnable=false;
-
+float temperature;
+float humidity;
 char charBuffer[32];
 
-byte counter = 1;
-unsigned long counterWaiter;
-bool counterReq = false;
-
-// Variables
-long wiFiConnTime = 0; // measure time till wifi connect success
-int wifiConnectCounter, loopCounter, httpConnectCounter = 0;
+int wifiConnectCounter = 0;
 
 // Pin14: VCC for Sensors
 const int GPIO_Power_Sensors = 14;
 
 
-int min(int x, int y) {if (x<y) return x; else return y;}
-int max(int x, int y) {if (x>y) return x; else return y;}
 // ------------------------
 void setup() {
-  
    Serial.begin(115200);   
 
-   Serial.println("ESPtoFHEM starts...");
+   if (debugOutput) Serial.println("ESP8266 starts...");
 
    // power on sensors
    pinMode(GPIO_Power_Sensors, OUTPUT);
    digitalWrite(GPIO_Power_Sensors, HIGH);
-   
-   // wait for sensors to power on
-   delay(2000);
+   //disable internal LED
+   pinMode(2, OUTPUT); 
+   digitalWrite(2, HIGH );
+   long start = millis();
     
-   Serial.println("Wifi wait for connection");
+   if (debugOutput) Serial.println("Wifi wait for connection");
 
    system_deep_sleep_set_option(1); // alt. 2 to not recalibrate WiFi after wake up, 1 is more stable needs more power
    wifi_set_sleep_type (LIGHT_SLEEP_T);
@@ -153,7 +118,7 @@ void setup() {
    yield();
 
    WiFi.begin(ssid, password);
-   if (!dhcp) WiFi.config(ip, gateway, subnet);
+   WiFi.config(ip, gateway, subnet);
 
    // Wait for connection
    while (WiFi.status() != WL_CONNECTED) {
@@ -164,20 +129,58 @@ void setup() {
        if (debugOutput)  Serial.println("No WiFi connection, try again in " + String(REPEAT_INTERVAL) + " seconds");
        system_deep_sleep(REPEAT_INTERVAL * 1000 * 1000); // after deep sleep automtic reset
        delay(200); // give some time to activate sleep state
-       abort(); // just for convience, when disabling deep sleep
      }
-     Serial.print(".");
    }
-   wifiConnectCounter = 0;
    if (debugOutput) Serial.println("");
    if (debugOutput) Serial.println("WiFi connected");
    if (debugOutput) Serial.println("IP address: ");
    if (debugOutput) Serial.println(WiFi.localIP());
-   wiFiConnTime = millis(); // establish time for connection in milliseconds
    wifi_set_sleep_type (LIGHT_SLEEP_T);
 
-   Serial.println("Wifi Started");
+   if (debugOutput) Serial.println("Wifi Started");
+   client.disconnect();
+   //wait at least 3 seconds...
+   long wait = millis()-start;
+   if (debugOutput) Serial.println("WiFi connection time "+String(wait)+" ms");
+   if( wait < 3000)
+    delay(3000-wait);
+   //starting the sensor 
+   startSensors();
+   // connect to MQTT Broker
+   while (!client.connected()) {
+   if (client.connect((char *)TOPIC_ClientID,(char *)TOPIC_LastWill,1,1,(char *)"0"))  
+    {
+      if (debugOutput) Serial.println("MQTT is connected");
+      if(processDHT()) //read DHT22
+      {
+        yield();
+        processMqttItems();//send temp and hum to MQTT Broker
+        delay(500);
+      }
+    }
+    else
+    {
+      if (debugOutput) Serial.println("MQTT is not connected... retrying");
+      delay(200);
+    }
+  }
+    // disconnect from MQTT
+    client.disconnect();
+    if (debugOutput) Serial.println("MQTT is disconnected");
+    yield();
 
+    // disconnect from WiFi
+    WiFi.disconnect();
+    if (debugOutput) Serial.println("WiFi is disconnected");
+    yield();
+
+
+  // all done enable deep_sleep
+   long sleep_dur = REPEAT_INTERVAL * 1000 * 1000 - millis() * 1000;
+   if (debugOutput) Serial.println("sleeping for " + String(REPEAT_INTERVAL) + " seconds");
+   system_deep_sleep(sleep_dur);
+   if (debugOutput) Serial.println("ESP8266 run complete in "+String(millis())+" ms");
+   delay(1000);//waiting a little bit, deep sleep needs some time to start
 }
 
 void startSensors(void) {
@@ -194,21 +197,8 @@ boolean myPublish(char* topic, char* payload)
 
 // ------------------------
 // process DHT22
-void processDHT()
+boolean processDHT()
 {
-  // process after wait time or counter turn around 
-    
-  if (millis() < dhtWaiter && !DeepSleep)
-    return;
-        
-  dhtWaiter = millis() + max((REPEAT_INTERVAL * 1000),dht.getMinimumSamplingPeriod());
-
-  // process after wait time or counter turn around 
-  // if (millis() < dhtWaiter && abs(dhtWaiter-millis()) < 10000 )
-  //     return;
-        
-  // dhtWaiter = millis() + max(5000,dht.getMinimumSamplingPeriod()); 
-
   if (debugOutput) Serial.println("DHT22: +++DHT start");
   
   dht.readSensor();
@@ -216,208 +206,45 @@ void processDHT()
   {
     if (debugOutput) Serial.print("DHT22 Error: ");
     if (debugOutput) Serial.println(dht.getStatusString());
-    return;
+    return false;
   }
 
-  float temperature = dht.getTemperature();
-  
+  temperature = dht.getTemperature();
   if (isnan(temperature)) 
   {
     if (debugOutput) Serial.println("DHT22: Failed reading temperature from DHT");
   } 
-  else if (temperature != lastTemp || firstInit) {
-    lastTemp = temperature;
-    tempReq = true;
-    if (debugOutput) Serial.print("DHT22 Deg: "); Serial.println(temperature);
-  }
-
-  float humidity = dht.getHumidity();
-
+  if (debugOutput) Serial.print("DHT22 Deg: "); Serial.println(temperature);
+  
+  humidity = dht.getHumidity();
   if (isnan(humidity)) {
     if (debugOutput) Serial.println("DHT22: Failed reading humidity from DHT");
   } 
-  else if (humidity != lastHum || firstInit) 
-  {
-    lastHum = humidity;
-    humReq = true;
-
-    if (debugOutput) Serial.print("DHT22 Hum: "); Serial.println(humidity);
-  }
-
+  if (debugOutput) Serial.print("DHT22 Hum: "); Serial.println(humidity);
   if (debugOutput) Serial.println("DHT22: ---DHT done");
-
+  return true;
 }
 
 // ---------------------------
 // processes the items to be published
 void processMqttItems()
 {
-  char buffer[30];
   String strBuffer;
-  
-  if (counterReq)
-  {
-      counterReq=false;
-      strBuffer =  String(counter);
-      strBuffer.toCharArray(charBuffer,10);
-      myPublish((char *)TOPIC_CounterState,charBuffer);  
-      if (debugOutput) Serial.println("Action: update counter");  
-  }
+  strBuffer =  String(humidity);
+  strBuffer.toCharArray(charBuffer,10);
+  myPublish((char *)TOPIC_HumidityState,charBuffer);  
+  if (debugOutput) Serial.println("Action: update hum");  
   yield();
-
-  if (humReq)
-  {
-     humReq=false;
-     strBuffer =  String(lastHum);
-     strBuffer.toCharArray(charBuffer,10);
-     myPublish((char *)TOPIC_HumidityState,charBuffer);  
-     if (debugOutput) Serial.println("Action: update hum");  
-  }
-  yield();
-  
-  if (tempReq)
-  {
-     tempReq=false;
-     strBuffer =  String(lastTemp);
-     strBuffer.toCharArray(charBuffer,10);
-     myPublish((char *)TOPIC_TemperatureState,charBuffer);  
-     if (debugOutput) Serial.println("Action: update temp");  
-  }
-}
-// -------------------------------
-
-void processMQTT()
-{
  
-  int nextState=MqState;
+  strBuffer =  String(temperature);
+  strBuffer.toCharArray(charBuffer,10);
+  myPublish((char *)TOPIC_TemperatureState,charBuffer);  
+  if (debugOutput) Serial.println("Action: update temp");  
 
-  if (millis() < MqWaiter)
-    return;
-
-  if (MqState == ST_INIT)
-  {
-    if (debugOutput) Serial.println("MQTT: Init");
-    client.disconnect();
-    MqWaiter = millis() + 2000;
-    nextState = ST_CONNECT;
-
-  } else if (MqState == ST_CONNECT) {
-    if (debugOutput) Serial.println("MQTT: +Try to connect");
-    if (debugOutput) Serial.print("MQTT ClientID:");Serial.println(TOPIC_ClientID);
-    
-    callbackEnable=false;
-    
-    // clientID, user,password,Will-Topic, Qos, Retain, will msg
-    //if (client.connect((char *)TOPIC_ClientID))  
-     if (client.connect((char *)TOPIC_ClientID,(char *)TOPIC_LastWill,1,1,(char *)"0"))  
-    {
-      if (debugOutput) Serial.println("MQTT: connected");
-
-      nextState = ST_CONNECTED;
-    } else {
-      if (debugOutput) Serial.println("MQTT: not connected");
-      nextState = ST_INIT;
-    }
-  } else if (MqState >= ST_CONNECTED) {
-    // mqtt loop
-    if ( ! client.loop())
-    {
-      if (debugOutput) Serial.println("MQTT: disconnected");
-      nextState = ST_INIT;
-    }
-    else {
-      processMqttItems();
-      // if (debugOutput) Serial.println("MQTT: distributed");
-      nextState = ST_DISTRIBUTED;
-    }
-  }
-
-  if (nextState != MqState)
-  {
-    MqState = nextState;
-    if (debugOutput) Serial.print("MQTT new state:");
-    if (debugOutput) Serial.println(nextState);
-  }
-
-}  
-
-// -------------------------------
-// process the counter
-
-void processCounter()
-{
-   if (millis() < counterWaiter)
-    return;
-  
-   counterWaiter=millis()+5000;
-   counter++;
-   counterReq=true;
-  
 }
+// -------------------------------
 
 void loop() {
-  loopCounter++; // count loops for quality
-
-  if (firstInit) {
-    startSensors();
-  }
-  if (MqState >= ST_CONNECTED) {
-    processDHT();
-    yield();
-  }
-  processMQTT();
-  yield();
-  processCounter();
-  
-  firstInit = false;
-  
-  // all done enable deep_sleep
-
-  if (MqState == ST_DISTRIBUTED  && DeepSleep) {
-    firstInit = true;
-
-    // disconnect from MQTT
-    client.disconnect();
-    yield();
-
-    // disconnect from WiFi
-    WiFi.disconnect();
-    yield();
-
-    // power off sensors
-    pinMode(GPIO_Power_Sensors, INPUT);
-
-    long sleep_dur = REPEAT_INTERVAL * 1000 * 1000 - millis() * 1000;
-    Serial.println("sleeping for " + String(REPEAT_INTERVAL) + " seconds");
-    if (sleep_dur < 0) sleep_dur = REPEAT_INTERVAL;
-    system_deep_sleep(sleep_dur);
-    delay(1000);
-    abort();
-  }
-
-}
-
-// Helpers for math function
-double pow(double x,double y) {
-  double z , p=1;
-  //y<0 ? z=-y : z=y ;
-  if(y<0)
-    z = fabs(y);
-  else
-    z = y;
- 
-  for(int i=0; i<z ; ++i)
-  {
-    p *= x;
-  }
-
-  if(y<0)
-    return 1/p;
-  else
-    return p;
-}
-
-double fabs(double x) {
-  return( x<0 ?  -x :  x );
+  //not needed
 }
 
